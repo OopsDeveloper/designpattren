745cdee8ed228bd82f9f9ec0bf93ab754dd9c0cd
고용수 <ajemxkem@gmail.com>
2025-01-04 12:02:31 +0900

    스터디 SOLID and Iterator 패턴

--- src/main/java/com/oopsdev/designpattern/solidprinciple/openclosed/noncompliant/ReportGenerator.java
+++ src/main/java/com/oopsdev/designpattern/solidprinciple/openclosed/noncompliant/ReportGenerator.java
@@ -9,4 +9,10 @@ public class ReportGenerator {
         }
         // If we need to add another format, we have to modify this method.
     }
+
+    /*
+     * -문제점
+     * 새로운 케이스들이 추가되면서 이 메소드의 코드는 보다 장황해지고 복잡해진다.
+     * 그 과정에서 실수로 기존 과정에 영향을 주게 될 수도 있고 이 메소드가 이미 사용되던 곳들에도 부작용이 발생할 수 있다.
+     * */
 }
